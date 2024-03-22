2b67bb73ca22343629970ce8664e2bfed21d1626
cypirando <fabiocypriano@gmail.com>
2024-03-22 14:59:13 -0300

    feature: adicionando rotas

---
+++ src/routes/routes.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Home from './Home';
+import About from './About';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        {/* Componente da tela inicial */}
+        <Route index element={<Home />} />
+        {/* Componente de uma rota específica */}
+        <Route path="about" element={<About />} />
+        {/* Componente para quando não encontrar uma rota */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
