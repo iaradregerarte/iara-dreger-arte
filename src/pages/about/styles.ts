73659ae5c80236bd2d7ac84799213d59bf202595
cypriano <fabio.elias@botdesigner.io>
2024-03-23 22:25:55 -0300

    feature: add new components

--- src/pages/about/styles.ts
+++
@@ -1,7 +0,0 @@
-import { Layout as AntdLayout } from 'antd';
-import styled from 'styled-components';
-
-export const Layout = styled(AntdLayout)`
-  height: 100%;
-  background: transparent;
-`;
--- src/pages/contact/contact.tsx
+++
@@ -1,7 +0,0 @@
-import { Layout } from 'antd';
-
-export const Contact = () => (
-  <Layout hasSider>
-    <Layout>contacts</Layout>
-  </Layout>
-);
