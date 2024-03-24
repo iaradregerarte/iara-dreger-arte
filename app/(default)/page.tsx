92b589930ef875351b49aec63d662cf8324d18b0
cypriano <fabio.elias@botdesigner.io>
2024-03-24 00:56:42 -0300

    feature: criação de home e footer

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,19 +1,17 @@
 export const metadata = {
-  title: 'Home - Open PRO',
+  title: 'Iara Dreger Arte',
   description: 'Page description',
 }
 
 import Hero from '@/components/hero'
-import Features from '@/components/features'
 import Newsletter from '@/components/newsletter'
-import Zigzag from '@/components/zigzag'
 import Testimonials from '@/components/testimonials'
+import Zigzag from '@/components/zigzag'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Features />
       <Zigzag />
       <Testimonials />
       <Newsletter />
---
+++ components/img.tsx
@@ -0,0 +1,15 @@
+// components/Img.tsx
+
+import React from "react";
+import NextImage, { ImageProps } from "next/image";
+
+interface ImgProps extends ImageProps {
+  alt: string;
+}
+export const Img: React.FC<ImgProps> = ({ src, alt, ...props }) => {
+  return (
+    <div className="relative flex justify-center items-center" data-aos="fade-up" data-aos-delay="200">
+      <NextImage src={src} alt={alt} {...props} />
+    </div>
+  );
+};
