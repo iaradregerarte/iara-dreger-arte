d3bd8d9eba37547092db3fac67c6766f45552457
cypriano <fabio.elias@botdesigner.io>
2024-05-16 20:39:08 -0300

    Feat: adionando imagens

---
+++ components/image-data.ts
@@ -0,0 +1,303 @@
+import Untitled1 from "@/public/images/Untitled1.jpeg";
+import Untitled2 from "@/public/images/Untitled2.jpeg";
+
+import Untitled3 from "@/public/images/Untitled3.jpeg";
+import Untitled4 from "@/public/images/Untitled4.jpeg";
+import Untitled5 from "@/public/images/Untitled5.jpeg";
+import Untitled6 from "@/public/images/Untitled6.jpeg";
+import Untitled7 from "@/public/images/Untitled7.jpeg";
+import Untitled8 from "@/public/images/Untitled8.jpeg";
+import Untitled9 from "@/public/images/Untitled9.jpeg";
+import Untitled10 from "@/public/images/Untitled10.jpeg";
+
+import Untitled11 from "@/public/images/Untitled11.jpeg";
+import Untitled12 from "@/public/images/Untitled12.jpeg";
+import Untitled13 from "@/public/images/Untitled13.jpeg";
+import Untitled14 from "@/public/images/Untitled14.jpeg";
+import Untitled15 from "@/public/images/Untitled15.jpeg";
+import Untitled16 from "@/public/images/Untitled16.jpeg";
+import Untitled17 from "@/public/images/Untitled17.jpeg";
+import Untitled18 from "@/public/images/Untitled18.jpeg";
+
+import UntitledI from "@/public/images/UntitledI.jpeg";
+import UntitledII from "@/public/images/UntitledII.jpeg";
+import UntitledIII from "@/public/images/UntitledIII.jpeg";
+import UntitledIV from "@/public/images/UntitledIV.jpeg";
+import UntitledV from "@/public/images/UntitledV.jpeg";
+import UntitledVI from "@/public/images/UntitledVI.jpeg";
+import UntitledVII from "@/public/images/UntitledVII.jpeg";
+import UntitledVIII from "@/public/images/UntitledVIII.jpeg";
+import UntitledIX from "@/public/images/UntitledIX.jpeg";
+import UntitledX from "@/public/images/UntitledX.jpeg";
+import UntitledXI from "@/public/images/UntitledXI.jpeg";
+import UntitledXII from "@/public/images/UntitledXII.jpeg";
+import UntitledXIII from "@/public/images/UntitledXIII.jpeg";
+import UntitledXIV from "@/public/images/UntitledXIV.jpeg";
+
+export const metamorfose = [
+  {
+    imageSrc: Untitled1,
+    Untitled: "Untitled 1",
+    title: "Acrílica sobre Tela, 2022-24",
+    size: "1.0 x 1.00 m",
+    description: "Moldura branca tipo filete",
+    price: "R$ 3.250,00",
+  },
+  {
+    imageSrc: Untitled2,
+    Untitled: "Untitled 2",
+    title: "Acrílica sobre Tela, 2018-24",
+    size: "1.0 x 1.00 m",
+    description: "Moldura branca tipo filete",
+    price: "R$ 3.250,00",
+  },
+];
+
+export const deusAtomo = [
+  {
+    Untitled: "Untitled 3",
+    imageSrc: Untitled3,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 975,00",
+  },
+  {
+    Untitled: "Untitled 4",
+    imageSrc: Untitled4,
+    size: " 0.60 x 0.50 m",
+    title: "Óleo sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 975,00",
+  },
+  {
+    Untitled: "Untitled 5",
+    imageSrc: Untitled5,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 975,00",
+  },
+  {
+    Untitled: "Untitled 6",
+    imageSrc: Untitled6,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 600,00",
+  },
+  {
+    Untitled: "Untitled 7",
+    imageSrc: Untitled7,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2022-24",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 975,00",
+  },
+  {
+    Untitled: "Untitled 8",
+    imageSrc: Untitled8,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 975,00",
+  },
+  {
+    Untitled: "Untitled 9",
+    imageSrc: Untitled9,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 600,00",
+  },
+  {
+    Untitled: "Untitled 10",
+    imageSrc: Untitled10,
+    size: " 0.60 x 0.50 m",
+    title: "Óleo sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 975,00",
+  },
+];
+
+export const transmutacao = [
+  {
+    Untitled: "Untitled 11",
+    imageSrc: Untitled11,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 1.200,00",
+  },
+  {
+    Untitled: "Untitled 12",
+    imageSrc: Untitled12,
+    size: " 0.60 x 0.50 m",
+    title: "Óleo sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 1.200,00",
+  },
+  {
+    Untitled: "Untitled 13",
+    imageSrc: Untitled13,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 1.200,00",
+  },
+  {
+    Untitled: "Untitled 14",
+    imageSrc: Untitled14,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 600,00",
+  },
+  {
+    Untitled: "Untitled 15",
+    imageSrc: Untitled15,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 1.200,00",
+  },
+  {
+    Untitled: "Untitled 16",
+    imageSrc: Untitled16,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 1.200,00",
+  },
+  {
+    Untitled: "Untitled 17",
+    imageSrc: Untitled17,
+    size: " 0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023-24",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 1.200,00",
+  },
+  {
+    Untitled: "Untitled 18",
+    imageSrc: Untitled18,
+    size: " 0.60 x 0.50 m",
+    title: "Óleo sobre Tela, 2023-24",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 1.200,00",
+  },
+];
+
+export const acervo = [
+  {
+    Untitled: "Untitled I",
+    imageSrc: UntitledI,
+    size: "1.20 x 0.20 m",
+    title: "Acrílica sobre 6 Telas, 2023-24",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 780,00",
+  },
+  {
+    Untitled: "Untitled II",
+    imageSrc: UntitledII,
+    size: "0.60 x 0.50 m",
+    title: "Óleo sobre Tela, 2024",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 1.200,00",
+  },
+  {
+    Untitled: "Untitled III",
+    imageSrc: UntitledIII,
+    size: "0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023",
+    description: "Chassis duplo, tipo painel",
+    price: "R$ 600,00",
+  },
+  {
+    Untitled: "Untitled IV",
+    imageSrc: UntitledIV,
+    size: "0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023",
+    description: "",
+    price: "R$ 400,00",
+  },
+  {
+    Untitled: "Untitled V",
+    imageSrc: UntitledV,
+    size: "0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023",
+    description: "",
+    price: "R$ 400,00",
+  },
+  {
+    Untitled: "Untitled VI",
+    imageSrc: UntitledVI,
+    size: "0.60 x 0.50 m",
+    title: "Acrílica sobre Tela, 2023",
+    description: "",
+    price: "R$ 400,00",
+  },
+  {
+    Untitled: "Untitled VII",
+    imageSrc: UntitledVII,
+    size: "1.40 x 1.40 m",
+    title: "Acrílica sobre Lona, 2016-2023",
+    description: "Painel",
+    price: "R$ 4.550,00",
+  },
+  {
+    Untitled: "Untitled VIII",
+    imageSrc: UntitledVIII,
+    size: "1.00 x 1.00 m",
+    title: "Acrílica sobre Tela, 2022",
+    description: "Moldura branca tipo filete",
+    price: "R$ 2.000,00",
+  },
+  {
+    Untitled: "Untitled IX",
+    imageSrc: UntitledIX,
+    size: "1.50 x 0.86 m",
+    title: "Acrílica sobre Lona, 2016-2022",
+    description: "Painel, Moldura branca tipo filete",
+    price: "R$ 4.190,00",
+  },
+  {
+    Untitled: "Untitled X",
+    imageSrc: UntitledX,
+    size: "1.00 x 1.00 m",
+    title: "Acrílica sobre Tela, 2022",
+    description: "Moldura branca tipo filete",
+    price: "R$ 2.000,00",
+  },
+  {
+    Untitled: "Untitled XI",
+    imageSrc: UntitledXI,
+    size: "1.42 x 1.00 m",
+    title: "Pigmento, PVA, Acrílica, Carvão e Grafite sobre Lona, 2016-2023",
+    description: "Painel",
+    price: "R$ 2.840,00",
+  },
+  {
+    Untitled: "Untitled XII",
+    imageSrc: UntitledXII,
+    size: "1.50 x 1.06 m",
+    title: "Pigmento, PVA, Acrílica e Carvão sobre Lona, 2016-2022",
+    description: "Painel com moldura branca tipo filete",
+    price: "R$ 3.018,00",
+  },
+  {
+    Untitled: "Untitled XIII",
+    imageSrc: UntitledXIII,
+    size: "1.42 x 1.00 m",
+    title: "Pigmento, Acrílica e Óleo sobre Lona, 2017-2018",
+    description: "Painel",
+    price: "R$ 2.840,00",
+  },
+  {
+    Untitled: "Untitled XIV",
+    imageSrc: UntitledXIV,
+    size: "0.64 x 0.47 m",
+    title: "Acrílica sobre Madeira com Chassi, 2020",
+    description: "",
+    price: "R$ 400,00",
+  },
+];
