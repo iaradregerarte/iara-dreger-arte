31da9ff2e62b364ddfd7a9c3fea8e6f6d0bbe278
cypriano <fabio.elias@botdesigner.io>
2024-05-21 19:20:11 -0300

    Feat: muda copy

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -107,7 +107,7 @@ export const Footer = () => {
             </ul>
           </div>
           <div className="text-gray-400 text-sm mr-4 pt-10 pb-10 pl-38">
-            &copy; Copyright (c) 2024
+            &copy; Copyright (c) 2024&nbsp;
             <a
               href="https://cypirando.com.br/"
               target="_blank"
