54a854ce947c7a3b42390649b67712039bb203d2
cypriano <fabio.elias@botdesigner.io>
2024-06-03 16:33:22 -0300

    Feat: ajuste https

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+"use client";
 import './css/style.css'
 
 import { Inter, Architects_Daughter } from 'next/font/google'
-
-import Header from '@/components/ui/header'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Banner from '@/components/banner'
 
 const inter = Inter({
@@ -28,11 +29,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.location.hostname.startsWith('www.')) {
+      router.replace(window.location.href.replace(/^https?:\/\/(?:www\.)?(.+)$/, 'https://$1'));
+    }
+  }, [router]);
+
   return (
     <html lang="en">
       <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden">
-          {/* <Header /> */}
           {children}
           <Banner />
         </div>
@@ -40,4 +48,3 @@ export default function RootLayout({
     </html>
   )
 }
- 
