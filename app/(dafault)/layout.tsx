30b89dbab83fd66a5057d1a4752440dd32bbd67b
cypriano <fabio.elias@botdesigner.io>
2024-06-03 16:17:51 -0300

    Feat: ajuste responsivo

--- app/(dafault)/layout.tsx
+++ app/(dafault)/layout.tsx
@@ -1,35 +1,37 @@
 "use client";
-
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 import { Footer } from "@/components/ui/footer";
-// import { PageIllustration } from "@/components/page-illustration";
 
 export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
   useEffect(() => {
+    if (typeof window !== 'undefined' && window.location.hostname.startsWith('www.')) {
+      router.replace(window.location.href.replace(/^https?:\/\/(?:www\.)?(.+)$/, 'https://$1'));
+    }
+
     AOS.init({
       once: true,
       disable: "phone",
       duration: 600,
       easing: "ease-out-sine",
     });
-  });
+  }, [router]);
 
   return (
     <>
       <main className="grow">
-        {/* <PageIllustration /> */}
-
         {children}
       </main>
-
       <Footer />
     </>
   );
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const nextConfig = {
   basePath: "/iara-dreger-arte",
-  // assetPrefix: "/iara-dreger-arte",
 };
 
 module.exports = nextConfig;
