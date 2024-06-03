f3402f956c6f187899890dde8a89ae0e2aebf959
cypriano <fabio.elias@botdesigner.io>
2024-06-03 17:11:13 -0300

    Feat: ajuste https-1

--- app/(dafault)/page.tsx
+++ app/(dafault)/page.tsx
@@ -1,18 +1,35 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import Hero from "@/components/hero";
+import { Zigzag } from "@/components/zigzag";
+
 export const metadata = {
   title: "Iara Dreger Arte",
   description: "Page description",
 };
 
-import Hero from "@/components/hero";
-import { Newsletter } from "@/components/newsletter";
-import { Zigzag } from "@/components/zigzag";
-
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      window.location.hostname.startsWith("www.")
+    ) {
+      router.replace(
+        window.location.href.replace(
+          /^https?:\/\/(?:www\.)?(.+)$/,
+          "https://$1"
+        )
+      );
+    }
+  }, [router]);
+
   return (
     <>
       <Hero />
       <Zigzag />
-      {/* <Newsletter /> */}
     </>
   );
 }
