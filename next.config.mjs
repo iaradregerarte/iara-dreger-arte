92f9dd9b409abbac158cf3ceb635756a0027f828
cypriano <fabio.elias@botdesigner.io>
2024-06-28 11:42:41 -0300

    feature:six commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,8 @@
 const nextConfig = {
   output: "export",
   images: {
-    unoptimized: true, // Isso permite servir imagens diretamente da pasta `/images`
+    unoptimized: true,
   },
-  // basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
-  // assetPrefix: process.env.URL ? process.env.URL : undefined,
 };
 
 export default nextConfig;
